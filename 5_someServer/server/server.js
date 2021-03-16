a8477b6edf3c51d91b0d83784c29e0938d027f96
Chris <chris@ducklabs.ca>
2021-03-16 13:58:37 -0600

    Some tweaks and section 5 for some server

---
+++ 5_someServer/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app= express()
+app.use(cors())
+
+var server = app.listen(5150, function(){
+    var port = server.address().port
+    console.log('Server started on', port)
+})
+
+app.get('/', function(req,res){
+    res.send('Server is up and running.')
+})
+
+var index = 0
+app.get('/index', function(req, res){
+    res.send(index.toString())
+})
